b9b270168e42ecd6b85ff280e7430c28ebcc94ac
Raxit Singh <raxitsingh4321@gmail.com>
2024-10-17 12:28:24 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,61 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { Toaster } from "@/components/ui/toaster";
+import "./globals.css";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "react-datepicker/dist/react-datepicker.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Meetup app",
+  description: "Video calling app",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <ClerkProvider
+        appearance={{
+          baseTheme: dark,
+          layout: {
+            logoImageUrl: "/icons/logo.svg",
+          },
+
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1c1f2e",
+            colorInputBackground: "#252a41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} bg-dark-2 antialiased`}
+        >
+          {children}
+          <Toaster />
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
